5317b24cc56eb7c2b12434741b718500f60ada94
Miguel Langarano <miguelangarano@gmail.com>
2023-09-14 20:57:27 -0500

    fix cypress config to support e2e_smoke spec patttern

--- examples/webapp/cypress.config.ts
+++ examples/webapp/cypress.config.ts
@@ -2,29 +2,29 @@ import { defineConfig } from "cypress";
 import currents from "cypress-cloud/plugin";
 
 module.exports = defineConfig({
-  video: true,
-  e2e: {
-    projectId: !!(process.env.GITHUB_ACTION || process.env.CIRCLE_BRANCH)
-      ? "Ij0RfK"
-      : "l4zuz8",
-    baseUrl: "https://todomvc.com/examples/vanillajs",
-    supportFile: "cypress/support/e2e.ts",
-    specPattern: "cypress/e2e/*.js",
-    setupNodeEvents(on, config) {
-      require("@cypress/grep/src/plugin")(config);
-      require("cypress-terminal-report/src/installLogsPrinter")(on);
-      return currents(on, config);
-    },
-  },
+	video: true,
+	e2e: {
+		projectId: !!(process.env.GITHUB_ACTION || process.env.CIRCLE_BRANCH)
+			? "Ij0RfK"
+			: "l4zuz8",
+		baseUrl: "https://todomvc.com/examples/vanillajs",
+		supportFile: "cypress/support/e2e.ts",
+		specPattern: ["cypress/e2e/*.js", "cypress/e2e_smoke/*.js"],
+		setupNodeEvents(on, config) {
+			require("@cypress/grep/src/plugin")(config);
+			require("cypress-terminal-report/src/installLogsPrinter")(on);
+			return currents(on, config);
+		},
+	},
 
-  component: {
-    specPattern: ["pages/__tests__/*.spec.tsx"],
-    setupNodeEvents(on, config) {
-      return currents(on, config);
-    },
-    devServer: {
-      framework: "next",
-      bundler: "webpack",
-    },
-  },
+	component: {
+		specPattern: ["pages/__tests__/*.spec.tsx"],
+		setupNodeEvents(on, config) {
+			return currents(on, config);
+		},
+		devServer: {
+			framework: "next",
+			bundler: "webpack",
+		},
+	},
 });
